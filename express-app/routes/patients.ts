18c4cfd6693d65d954b5111dd402b0d69a8d1e16
Jonne Kanerva <8905786+Jamb000h@users.noreply.github.com>
2024-08-28 15:38:49 +0300

    New 9.14

--- express-app/routes/patients.ts
+++ express-app/routes/patients.ts
@@ -4,6 +4,7 @@ import {
   addPatient,
 } from "../services/patientService";
 import { toNewPatient } from "../utils";
+import { z } from "zod";
 
 const router = express.Router();
 
@@ -17,11 +18,11 @@ router.post("/", (req, res) => {
     const patient = addPatient(newPatient);
     res.json(patient);
   } catch (error: unknown) {
-    let errorMessage = "Something went wrong.";
-    if (error instanceof Error) {
-      errorMessage += " Error: " + error.message;
+    if (error instanceof z.ZodError) {
+      res.status(400).send({ error: error.issues });
+    } else {
+      res.status(400).send({ error: "unknown error" });
     }
-    res.status(400).send(errorMessage);
   }
 });
 
--- express-app/utils/index.ts
+++ express-app/utils/index.ts
@@ -1,82 +1,23 @@
+import { z } from "zod";
 import { Gender } from "../enums";
 import { Patient } from "../types";
 import { v1 as uuid } from "uuid";
 
-const isString = (text: unknown): text is string => {
-  return typeof text === "string" || text instanceof String;
-};
-
-const isGender = (param: string): param is Gender => {
-  return Object.values(Gender)
-    .map((v) => v.toString())
-    .includes(param);
-};
-
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const parseName = (name: unknown): string => {
-  if (!isString(name)) {
-    throw new Error("Incorrect or missing name");
-  }
-
-  return name;
-};
-
-const parseSsn = (ssn: unknown): string => {
-  if (!isString(ssn)) {
-    throw new Error("Incorrect or missing ssn");
-  }
-
-  return ssn;
-};
-
-const parseGender = (gender: unknown): Gender => {
-  if (!isString(gender) || !isGender(gender)) {
-    throw new Error("Incorrect or missing gender");
-  }
-
-  return gender;
-};
-
-const parseDateOfBirth = (dateOfBirth: unknown): string => {
-  if (!isString(dateOfBirth) || !isDate(dateOfBirth)) {
-    throw new Error("Incorrect or missing dateOfBirth");
-  }
-
-  return dateOfBirth;
-};
-
-const parseOccupation = (occupation: unknown): string => {
-  if (!isString(occupation)) {
-    throw new Error("Incorrect or missing occupation");
-  }
-
-  return occupation;
-};
-
 export const toNewPatient = (object: unknown): Patient => {
-  if (!object || typeof object !== "object") {
-    throw new Error("Incorrect or missing data");
-  }
-  if (
-    "name" in object &&
-    "dateOfBirth" in object &&
-    "ssn" in object &&
-    "gender" in object &&
-    "occupation" in object
-  ) {
-    const newEntry: Patient = {
-      id: uuid(),
-      name: parseName(object.name),
-      dateOfBirth: parseDateOfBirth(object.dateOfBirth),
-      ssn: parseSsn(object.ssn),
-      gender: parseGender(object.gender),
-      occupation: parseOccupation(object.occupation),
-    };
+  const newPatientSchema = z.object({
+    name: z.string(),
+    gender: z.nativeEnum(Gender),
+    dateOfBirth: z.string().date(),
+    ssn: z.string(),
+    occupation: z.string(),
+  });
 
-    return newEntry;
-  }
-  throw new Error("Incorrect data: some fields are missing");
+  const newPatient = newPatientSchema.parse(object);
+
+  const newEntry: Patient = {
+    ...newPatient,
+    id: uuid(),
+  };
+
+  return newEntry;
 };
