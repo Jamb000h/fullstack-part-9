d4650b1ce21bf58bc3cfc640b89aa2b7959a0001
Jonne Kanerva <8905786+Jamb000h@users.noreply.github.com>
2024-08-28 16:50:01 +0300

    9.24

--- patientor/src/components/SinglePatient.tsx
+++ patientor/src/components/SinglePatient.tsx
@@ -30,6 +30,23 @@ const SinglePatient = () => {
       <p>Gender: {patient.gender}</p>
       <p>SSN: {patient.ssn}</p>
       <p>Occupation: {patient.occupation}</p>
+      {patient.entries.length > 0 && (
+        <>
+          <h2>Entries</h2>
+          {patient.entries.map((entry, i) => {
+            return (
+              <div className="entry" key={i}>
+                {entry.date} <i>{entry.description}</i>
+                <ul>
+                  {entry.diagnosisCodes?.map((diagnosisCode) => (
+                    <li key={diagnosisCode}>{diagnosisCode}</li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+        </>
+      )}
     </div>
   );
 };
